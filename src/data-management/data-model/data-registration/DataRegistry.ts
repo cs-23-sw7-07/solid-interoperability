1716ff1fee41ef4afa61cac4f9ec8b413aed628e
S3JER <72788875+S3JER@users.noreply.github.com>
2023-10-23 14:53:01 +0200

    #3

---
+++ src/data-management/data-model/data-registration/DataRegistry.ts
@@ -0,0 +1,10 @@
+import { DataRegistration } from "./data-registration";
+
+class DataRegistry {
+    hasDataRegistration: DataRegistration[];
+    constructor(
+        hasDataRegistration: DataRegistration[]
+    ) {
+        this.hasDataRegistration = hasDataRegistration;
+    }
+}
