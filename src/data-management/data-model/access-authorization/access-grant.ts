6e46a25430f15e3c787a51fdbb4f004cd79369c6
S3JER <72788875+S3JER@users.noreply.github.com>
2023-10-24 10:44:14 +0200

    #3

--- src/data-management/data-model/access-authorization/access-grant.ts
+++ src/data-management/data-model/access-authorization/access-grant.ts
@@ -6,7 +6,7 @@ export class AccessGrant {
     grantedBy: SocialAgent;
     grantedAt: Date;
     grantee: Agent;
-    hasAccessNeedGroup: string;
+    hasAccessNeedGroup: AccessNeedGroup;
     hasDataGrant: DataGrant[];
 
     constructor(
@@ -14,7 +14,7 @@ export class AccessGrant {
         grantedBy: SocialAgent,
         grantedAt: Date,
         grantee: Agent,
-        hasAccessNeedGroup: string,
+        hasAccessNeedGroup: AccessNeedGroup,
         hasDataGrant: DataGrant[]
     ) {
         this.id = id;
