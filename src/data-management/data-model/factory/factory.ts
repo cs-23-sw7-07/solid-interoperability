0c7bb8842cf65e18cd9ed728ced7cbaeef2bf7a6
Mikkel-Boje <milars20@student.aau.dk>
2023-10-30 15:17:59 +0100

    made some comments

--- src/data-management/data-model/factory/factory.ts
+++ src/data-management/data-model/factory/factory.ts
@@ -11,7 +11,16 @@ const PREFIXES = {
     }
 };
 
+/**
+ * This factory is used for `RDF` creation via. the `createRdf` function.
+ * It uses the `N3.writer` to create a turtle (.ttl) file.
+ */
 export class rdfFactory {
+    /**
+     * 
+     * @param thing is class implementing the `ItoRdf` interface
+     * @returns a `Promise` which if furfulled contains a turtle file, otherwise an error which needs handling
+     */
     createRdf(thing: ItoRdf) {
         return new Promise((resolve, reject) => {
             const writer = new N3.Writer(PREFIXES, { format: "Turtle" })
