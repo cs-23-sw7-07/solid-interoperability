0f02aaa6dc34e4528d066cdb2f63525f79778c2f
Simon Deleuran Laursen <69715502+Laursen79@users.noreply.github.com>
2023-11-06 09:45:43 +0100

    Fixed linting and spelling mistakes.

--- src/data-management/data-model/data-registration/data-registration.ts
+++ src/data-management/data-model/data-registration/data-registration.ts
@@ -1,69 +1,69 @@
-import N3 from 'n3';
+import N3 from "n3";
 import { Agent, SocialAgent } from "../agent";
 
 const { DataFactory } = N3;
 const { namedNode, literal, quad } = DataFactory;
 
 export class DataRegistration {
-    id: string;
-    storedAt: string;
-    registeredBy: SocialAgent;
-    registeredWith: Agent;
-    registeredAt: Date;
-    updatedAt: Date;
-    registeredShapeTree: string;
+  id: string;
+  storedAt: string;
+  registeredBy: SocialAgent;
+  registeredWith: Agent;
+  registeredAt: Date;
+  updatedAt: Date;
+  registeredShapeTree: string;
 
-    constructor(
-        id: string,
-        storedAt: string,
-        registeredBy: SocialAgent,
-        registeredWith: Agent,
-        registeredAt: Date,
-        updatedAt: Date,
-        registeredShapeTree: string
-    ) {
-        this.id = id;
-        this.storedAt = storedAt;
-        this.registeredBy = registeredBy;
-        this.registeredWith = registeredWith;
-        this.registeredAt = registeredAt;
-        this.updatedAt = updatedAt;
-        this.registeredShapeTree = registeredShapeTree;
-    }
+  constructor(
+    id: string,
+    storedAt: string,
+    registeredBy: SocialAgent,
+    registeredWith: Agent,
+    registeredAt: Date,
+    updatedAt: Date,
+    registeredShapeTree: string,
+  ) {
+    this.id = id;
+    this.storedAt = storedAt;
+    this.registeredBy = registeredBy;
+    this.registeredWith = registeredWith;
+    this.registeredAt = registeredAt;
+    this.updatedAt = updatedAt;
+    this.registeredShapeTree = registeredShapeTree;
+  }
 
-    toRdf(writer: N3.Writer): void {
-        const subject = `${this.storedAt}/${this.id}/`
-        const subjectNode = namedNode(subject)
+  toRdf(writer: N3.Writer): void {
+    const subject = `${this.storedAt}/${this.id}/`;
+    const subjectNode = namedNode(subject);
 
-        writer.addQuad(
-            subjectNode,
-            namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-            namedNode('interop:DataRegistration')
-        );
-        writer.addQuad(
-            subjectNode,
-            namedNode('interop:registeredBy'),
-            namedNode(this.registeredBy.getWebID())
-        );
-        writer.addQuad(
-            subjectNode,
-            namedNode('interop:registeredWith'),
-            namedNode(this.registeredWith.getWebID())
-        );
-        writer.addQuad(
-            subjectNode,
-            namedNode('interop:registeredAt'),
-            literal(this.registeredAt.toISOString(), namedNode("xsd:dateTime"))
-        );
-        writer.addQuad(
-            subjectNode,
-            namedNode('interop:updatedAt'),
-            literal(this.updatedAt.toISOString(), namedNode("xsd:dateTime"))
-        );
-        writer.addQuad(
-            subjectNode,
-            namedNode('interop:registeredShapeTree'),
-            namedNode(this.registeredShapeTree)
-        );
-    }
+    writer.addQuad(
+      subjectNode,
+      namedNode("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+      namedNode("interop:DataRegistration"),
+    );
+    writer.addQuad(
+      subjectNode,
+      namedNode("interop:registeredBy"),
+      namedNode(this.registeredBy.getWebID()),
+    );
+    writer.addQuad(
+      subjectNode,
+      namedNode("interop:registeredWith"),
+      namedNode(this.registeredWith.getWebID()),
+    );
+    writer.addQuad(
+      subjectNode,
+      namedNode("interop:registeredAt"),
+      literal(this.registeredAt.toISOString(), namedNode("xsd:dateTime")),
+    );
+    writer.addQuad(
+      subjectNode,
+      namedNode("interop:updatedAt"),
+      literal(this.updatedAt.toISOString(), namedNode("xsd:dateTime")),
+    );
+    writer.addQuad(
+      subjectNode,
+      namedNode("interop:registeredShapeTree"),
+      namedNode(this.registeredShapeTree),
+    );
+  }
 }
