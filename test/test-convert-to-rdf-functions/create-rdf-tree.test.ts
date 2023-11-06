cc7e734e37ff63bf691bf849df09847c45c7061b
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2023-11-06 14:40:47 +0100

    Fix test

--- test/test-convert-to-rdf-functions/create-rdf-tree.test.ts
+++ test/test-convert-to-rdf-functions/create-rdf-tree.test.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import { ExampleInstances } from "../test-case"
 import { RdfFactory } from '../../src/data-management/data-model/factory/rdfFactory';
+import * as ExampleInstances from '../test-case'
 
 
 const PATH_TO_RDFS_EXAMPLES = join(__dirname, "../rdfs-examples")
@@ -12,9 +12,9 @@ function getExpectedRDFFromFile(path: string): string {
 
 test(
     "Test-toRdfSocialAgentRegistration-c4562da9", async () => {
-        let expected = getExpectedRDFFromFile("agents/c4562da9SocialAgentRegistration/c4562da9.ttl")
+        const expected = getExpectedRDFFromFile("agents/c4562da9SocialAgentRegistration/c4562da9.ttl")
 
-        let actual = await new RdfFactory().create(ExampleInstances.socialAgentRegistrationc4562da9)
+        const actual = await new RdfFactory().create(ExampleInstances.socialAgentRegistrationc4562da9)
 
         expect(actual).toBe(expected)
     }
@@ -22,9 +22,9 @@ test(
 
 test(
     "Test-toRdfApplicationRegistration-2f2f3628", async () => {
-        let expected = getExpectedRDFFromFile("agents/2f2f3628ApplicationRegistration/2f2f3628.ttl")
+        const expected = getExpectedRDFFromFile("agents/2f2f3628ApplicationRegistration/2f2f3628.ttl")
 
-        let actual = await new RdfFactory().create(ExampleInstances.applicationRegistration2f2f3628)
+        const actual = await new RdfFactory().create(ExampleInstances.applicationRegistration2f2f3628)
 
         expect(actual).toBe(expected)
     }
@@ -34,9 +34,9 @@ test.each([
     { name: '8501f084', expect_rdf_file_path: "data/8501f084DataRegistration/8501f084.ttl", instance: ExampleInstances.dataRegistration8501f084 },
     { name: 'df4ab227', expect_rdf_file_path: "data/df4ab227DataRegistration/df4ab227.ttl", instance: ExampleInstances.dataRegistrationdf4ab227 }
 ])('Test-toRdfDataRegistration-%s', async (arg) => {
-    let expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
+    const expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
 
-    let actual = await new RdfFactory().create(arg.instance)
+    const actual = await new RdfFactory().create(arg.instance)
 
     expect(actual).toBe(expected)
 })
@@ -48,9 +48,9 @@ test.each([
     { name: 'b42228af', expect_rdf_file_path: "agents/c4562da9SocialAgentRegistration/b42228afDataGrant.ttl", instance: ExampleInstances.dataGrantb42228af },
     { name: '2aa21a8c', expect_rdf_file_path: "agents/c4562da9SocialAgentRegistration/2aa21a8cDataGrant.ttl", instance: ExampleInstances.dataGrant2aa21a8c },
 ])('Test-toRdfDataGrant-%s', async (arg) => {
-    let expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
+    const expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
 
-    let actual = await new RdfFactory().create(arg.instance)
+    const actual = await new RdfFactory().create(arg.instance)
 
     expect(actual).toBe(expected)
 })
@@ -60,9 +60,9 @@ test.each([
     { name: '27eae14b', expect_rdf_file_path: "agents/2f2f3628ApplicationRegistration/27eae14bAccessGrant.ttl", instance: ExampleInstances.accessGrant27eae14b },
     { name: 'b6e125b8', expect_rdf_file_path: "agents/c4562da9SocialAgentRegistration/b6e125b8AccessGrant.ttl", instance: ExampleInstances.accessGrantb6e125b8 },
 ])('Test-toRdfAccessGrant-%s', async (arg) => {
-    let expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
+    const expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
 
-    let actual = await new RdfFactory().create(arg.instance)
+    const actual = await new RdfFactory().create(arg.instance)
 
     expect(actual).toBe(expected)
 })
@@ -72,9 +72,9 @@ test.each([
     { name: '54a1b6a0', expect_rdf_file_path: "authorization/54a1b6a0DataAuth.ttl", instance: ExampleInstances.dataAuthorization54a1b6a0 },
     { name: '23a123bd', expect_rdf_file_path: "authorization/23a123bdDataAuth.ttl", instance: ExampleInstances.dataAuthorization23a123bd },
 ])('Test-toRdfDataAuthorization-%s', async (arg) => {
-    let expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
+    const expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
 
-    let actual = await new RdfFactory().create(arg.instance)
+    const actual = await new RdfFactory().create(arg.instance)
 
     expect(actual).toBe(expected)
 })
@@ -84,9 +84,9 @@ test.each([
     { name: 'e2765d6d', expect_rdf_file_path: "authorization/e2765d6dAccessAuth.ttl", instance: ExampleInstances.accessAuthorizatione2765d6d },
     { name: '47e07897', expect_rdf_file_path: "authorization/47e07897AccessAuth.ttl", instance: ExampleInstances.accessAuthorizatione47e07897 },
 ])('Test-toRdfDataAuthorization-%s', async (arg) => {
-    let expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
+    const expected = getExpectedRDFFromFile(arg.expect_rdf_file_path)
 
-    let actual = await new RdfFactory().create(arg.instance)
+    const actual = await new RdfFactory().create(arg.instance)
 
     expect(actual).toBe(expected)
 })
