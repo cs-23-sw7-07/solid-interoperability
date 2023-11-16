096f9d4734b9aae1e9b2fb38f479bab3b9fa2bf9
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2023-11-16 21:43:19 +0100

    able to make as package

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export * from "./data-management/data-model/agent-registration/agent-registration"
+export * from "./data-management/data-model/agent-registration/application-registration"
+export * from "./data-management/data-model/agent-registration/social-agent-registration"
+
+export * from "./data-management/data-model/authorization/access-auhorization"
+export * from "./data-management/data-model/authorization/access-grant"
+export * from "./data-management/data-model/authorization/access-mode"
+export * from "./data-management/data-model/authorization/data-authorization"
+export * from "./data-management/data-model/authorization/data-grant"
+export * from "./data-management/data-model/authorization/grant-scope"
+
+export * from "./data-management/data-model/data-registration/data-registration"
+export * from "./data-management/data-model/agent"
+export * from "./data-management/data-model/rdf"
+export * from "./data-management/data-model/registration"
+
+export * from "./data-management/data-model/factory/rdfFactory"
