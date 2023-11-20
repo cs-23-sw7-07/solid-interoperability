d7c00f7391f356c87db90eef19c88d4af57ff3cd
Mikkel-Boje <milars20@student.aau.dk>
2023-11-20 14:45:22 +0100

    Added some docs for users to have an easier time implementing the DataGrantBuilder

--- src/data-management/data-model/authorization/data/IDataGrantBuilder.ts
+++ src/data-management/data-model/authorization/data/IDataGrantBuilder.ts
@@ -5,11 +5,29 @@ import { DataGrant } from "./data-grant";
 import { DataInstance } from "./data-instance";
 
 export interface IDataGrantBuilder {
+  /**
+   * The `generateId()` function should generate a random uuid.
+   */
   generateId(): string;
+
+  /**
+   * This function need to return **all** registrations in which the pod owner has access to.
+   * @param registeredShapeTree 
+   * @param dataOwner this parameter should not be supplied if the `scopeOfGrant` is not of type `interop:All` and is of type `interop:AllFromAgent`
+   */
   getAllDataRegistrations(
     registeredShapeTree: string,
     dataOwner?: SocialAgent,
   ): Promise<DataRegistration[]>;
+
+  /**
+   * This function should use the Data Authorization to find all `DataGrant(s)` from the inherited Data Authorization. \
+   * This can be implemented using the `.toDataGrant` method on all `DataAuthorization` objects.
+   */
   getInheritedDataGrants(auth: DataAuthorization): Promise<DataGrant[]>;
+
+  /**
+   * @returns should return a list of `DataInstance(s)` drawn from the shape tree.
+   */
   getDataInstances(registeredShapeTree: string): DataInstance[];
 }
