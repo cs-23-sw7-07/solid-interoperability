6aa4eac4093d671eef56bbcf2826510e7d954e86
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2023-12-07 20:41:49 +0100

    update

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['lib/'],
+  globalSetup: './test/setup/setup.js',
+  globalTeardown: './test/setup/teardown.js',
 };
