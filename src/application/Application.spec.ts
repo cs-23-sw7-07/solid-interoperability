c3db22ad45c2c3902c259cd702c2b3ab5dbe1b25
darkros <madsmario@gmail.com>
2023-12-14 10:18:07 +0100

    Can use Shapetree

--- src/application/Application.spec.ts
+++ src/application/Application.spec.ts
@@ -30,9 +30,36 @@ describe("Application", () => {
     })
 
     expressApp = express();
+    expressApp.use(express.static(path.join(__dirname, "../../test/resources")));
     expressApp.get("/", appRouter);
     expressApp.use("/ldp", testRouter);
     expressApp.listen(3002);
+
+  });
+
+  it("should be able to fetch RDF file", async () => {
+    let res1: Response = await fetch("http://localhost:3002/testThing1.ttl", {headers: {Accepts: "text/turtle"}});
+    let res2: Response = await fetch("http://localhost:3002/shapes.shex", {headers: {Accepts: "text/shex"}});
+    expect(await res1.text()).toBe("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+        "@prefix localhost: <http://localhost:3001/ldp#>\n" +
+        "\n" +
+        "localhost:576520a6-af5a-4cf9-8b40-8b1512b59c73\\#Thing\n" +
+        "  a localhost:Task ;\n" +
+        "  rdfs:label \"Test task 1\" .")
+    expect(await res2.text()).toBe("PREFIX localhost: <http://localhost:3001/ldp#>\n" +
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+        "\n" +
+        "<#Test> {\n" +
+        "  a [localhost:Test] ;\n" +
+        "  rdfs:label xsd:string;\n" +
+        "  localhost:hasThing IRI *\n" +
+        "}\n" +
+        "\n" +
+        "<#Thing> {\n" +
+        "  a [localhost:Thing] ;\n" +
+        "  rdfs:label xsd:string\n" +
+        "}")
   });
 
   afterEach(() => {});
